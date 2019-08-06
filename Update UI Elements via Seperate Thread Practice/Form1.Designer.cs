6d9cbbc4a9414dbf01bc4723bb7642f41bf87e31
Christian Rodier <crodier@protonmail.com>
2019-08-05 20:38:48 -0600

    first commit

--- Update UI Elements via Seperate Thread Practice/Form1.Designer.cs
+++ Update UI Elements via Seperate Thread Practice/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(581, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Update UI Elements via Seperate Thread Practice/Form1.cs
+++ Update UI Elements via Seperate Thread Practice/Form1.cs
@@ -6,15 +6,56 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Update_UI_Elements_via_Seperate_Thread_Practice
-{
+{    
+
     public partial class Form1 : Form
     {
+        public delegate void delUpdateUITextBox(string text);
+
+        ThreadStart threadstart;
+        Thread myUpdateThread;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //instantiate thread start obj and pass in what method it will call
+            threadstart = new ThreadStart(GetTheThreadStarted);
+
+            myUpdateThread = new Thread(threadstart);
+
+            myUpdateThread.Name = "Second Thread";
+
+            myUpdateThread.Start();
+        }
+
+        //will get the thread started
+        private void GetTheThreadStarted()
+        {
+            delUpdateUITextBox DelupdateUITextBox = new delUpdateUITextBox(UpdateUITextBox);
+
+            this.textBox1.BeginInvoke(DelupdateUITextBox, "I was updated by from the thread: " + myUpdateThread.Name);
+            
+
+            for (int i = 0; i < 10; i++)
+            {
+                this.label1.BeginInvoke(DelupdateUITextBox, i.ToString());
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void UpdateUITextBox(string text)
+        {
+            this.textBox1.Text = text;
+            this.label1.Text = text;
         }
     }
 }
